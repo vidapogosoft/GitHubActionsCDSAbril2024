29bff65d5bfbda14215655a30c5acddad4b3fb5d
vidapogosoft <vidapogosoft@hotmail.com>
2024-04-24 20:17:48 -0500

    upload inicial
    codigo para probar ejecucion del job de github actions

---
+++ src/MyProject/Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace MyProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World folks!");
+            Console.WriteLine();
+            Console.WriteLine("Hello Github actions!");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to close");
+            Console.ReadKey();
+        }
+    }
+}
